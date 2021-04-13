1310b6342c8f56f3b69aef4b51412759b04a28e9
AnushkaAmte <anushkaamte@gmail.com>
2021-04-13 20:00:20 +0530

    added add user sql

--- add_user.php
+++ add_user.php
@@ -1,4 +1,58 @@
-<?php?>
+<?php
+include('db_connect.php');
+$email_id = '';
+$user_name = '';
+$roll_no = '';
+$department = '';
+$semester = '';
+$errors = array('email_id' => '', 'user_name' => '', 'roll_no' => '', 'department' => '', 'semester' => '');
+if (isset($_POST['submit'])) {
+    if (empty($_POST['email_id'])) {
+        $errors['email_id'] = "Email ID is required  <br />";
+    } else {
+        $email_id = $_POST['email_id'];
+    }
+    if (empty($_POST['user_name'])) {
+        $errors['user_name'] = "User name is required  <br />";
+    } else {
+        $user_name = $_POST['user_name'];
+    }
+    if (empty($_POST['roll_no'])) {
+        $errors['roll_no'] = "Roll Number is required  <br />";
+    } else {
+        $roll_no = $_POST['roll_no'];
+    }
+    if (empty($_POST['department'])) {
+        $errors['department'] = "Department's name is required  <br />";
+    } else {
+        $department = $_POST['department'];
+    }
+    if (empty($_POST['semester'])) {
+        $errors['semester'] = "Semester is required  <br />";
+    } else {
+        $semester = $_POST['semester'];
+    }
+
+    if (array_filter($errors)) {
+        echo 'Please fill a valid response';
+    } else {
+        $email_id = mysqli_real_escape_string($conn, $_POST['email_id']);
+        $user_name = mysqli_real_escape_string($conn, $_POST['user_name']);
+        $roll_no = $_POST['roll_no'];
+        $department = mysqli_real_escape_string($conn, $_POST['department']);
+        $semester =  $_POST['semester'];
+
+
+        $sql = "INSERT INTO `users` (email_id,user_name,roll_no,department,semester) VALUES ('$email_id','$user_name','$roll_no','$department','$semester')";
+
+        if (mysqli_query($conn, $sql)) {
+            header('Location: index.php');
+        } else {
+            echo 'An error occured: ' . mysqli_error($conn);
+        }
+    }
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -15,38 +69,39 @@
 <body>
     <?php include('navbar.php'); ?>
     <div class="card" style="width: 30rem; margin:auto; padding: 10px;">
-        <form action="add_user.php" method="GET" id='form'>
+        <form action="add_user.php" method="POST" id='form'>
 
             <div class="col md-3 align-items-center">
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="exampleInputPassword1" name='user_email' required>
+                    <label for="email_id" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email_id" name='email_id' value="<?php echo htmlspecialchars($email_id); ?>" required>
+                    <div class="invalid-feedback"><?php echo $errors['email_id']; ?></div>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='user_name' required>
+                    <label for="user_name" class="form-label">Name</label>
+                    <input type="text" class="form-control" id="user_name" name='user_name' value="<?php echo htmlspecialchars($user_name); ?>" required>
+                    <div class="invalid-feedback"><?php echo $errors['user_name']; ?></div>
 
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Roll Number</label>
-                    <input type="number" class="form-control" id="exampleInputPassword1" name='roll_no' required>
+                    <label for="roll_no" class="form-label">Roll Number</label>
+                    <input type="number" class="form-control" id="exampleInputPassword1" name='roll_no' value="<?php echo htmlspecialchars($roll_no); ?>" required>
+                    <div class="invalid-feedback"><?php echo $errors['roll_no']; ?></div>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Department</label>
-                    <input type="text" class="form-control" id="exampleInputPassword1" name='department' required>
+                    <label for="department" class="form-label">Department</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" name='department' value="<?php echo htmlspecialchars($department); ?>" required>
+                    <div class="invalid-feedback"><?php echo $errors['department']; ?></div>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Semester</label>
-                    <input type="number" class="form-control" id="exampleInputPassword1" name='semester' required>
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Dues</label>
-                    <input type="number" class="form-control" id="exampleInputPassword1" name='dues' required>
+                    <label for="semester" class="form-label">Semester</label>
+                    <input type="number" class="form-control" id="exampleInputPassword1" name='semester' value="<?php echo htmlspecialchars($semester); ?>" required>
+                    <div class="invalid-feedback"><?php echo $errors['semester']; ?></div>
                 </div>
 
                 <br>
                 <div class="text-center">
-                    <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                    <button type="submit" class="btn btn-primary btn-block" name="submit">Submit</button>
                 </div>
             </div>
             <br>
