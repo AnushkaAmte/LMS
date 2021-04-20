3c6e4c6d7fbd41ed5cace11fbe05091b0666712d
AnushkaAmte <anushkaamte@gmail.com>
2021-04-21 00:26:58 +0530

    added search bar

--- users.php
+++ users.php
@@ -27,44 +27,47 @@ mysqli_close($conn);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/favicon.ico">
 </head>
 
 <body>
     <?php include('navbar.php'); ?>
 
-    <div class="row">
-        <div class="col">
-            <h5>Users:</h5>
 
-            <div class="card card-body">
-                <table class="table table-sm">
-                    <tr>
-                        <th>Roll No</th>
-                        <th>Name</th>
-                        <th>Department</th>
-                        <th>Semester</th>
-                        <th>Email</th>
-                        <th>Update</th>
-                        <th>Delete</th>
-                    </tr>
-                    <?php foreach ($users as $user) : ?>
-                        <tr>
-                            <td><?php echo htmlspecialchars($user['roll_no']); ?></td>
-                            <td><?php echo htmlspecialchars($user['user_name']); ?></td>
-                            <td><?php echo htmlspecialchars($user['department']); ?></td>
-                            <td><?php echo htmlspecialchars($user['semester']); ?></td>
-                            <td><?php echo htmlspecialchars($user['email_id']); ?></td>
+    <h5>Users:</h5>
+    <div class="input-group" style="max-width: 200;">
+        <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+        <button type="button" class="btn btn-outline-primary">search</button>
+    </div>
+    <div class="card card-body">
+        <table class="table table-sm">
+            <tr>
+                <th>Roll No</th>
+                <th>Name</th>
+                <th>Department</th>
+                <th>Semester</th>
+                <th>Email</th>
+                <th>Update</th>
+                <th>Delete</th>
+            </tr>
+            <?php foreach ($users as $user) : ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($user['roll_no']); ?></td>
+                    <td><?php echo htmlspecialchars($user['user_name']); ?></td>
+                    <td><?php echo htmlspecialchars($user['department']); ?></td>
+                    <td><?php echo htmlspecialchars($user['semester']); ?></td>
+                    <td><?php echo htmlspecialchars($user['email_id']); ?></td>
 
-                            <td><a href="update_users.php?id=<?php echo $user['roll_no'] ?>" class="btn btn-sm btn-outline-primary">Update</a></td>
-                            <td>
-                                <form action="users.php" method="POST">
-                                    <input type="hidden" name="id_to_delete" value="<?php echo $user['roll_no']; ?>">
-                                    <input type="submit" name="delete" value="Delete" class="btn btn-sm btn-outline-danger">
-                                </form>
-                            </td>
-                            <!-- <td><a href="" class="btn btn-sm btn-outline-danger" data-toggle="modal" data-target="#exampleModal">Delete</a></td> -->
-                        </tr>
-                        <!-- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <td><a href="update_users.php?id=<?php echo $user['roll_no'] ?>" class="btn btn-sm btn-outline-primary">Update</a></td>
+                    <td>
+                        <form action="users.php" method="POST">
+                            <input type="hidden" name="id_to_delete" value="<?php echo $user['roll_no']; ?>">
+                            <input type="submit" name="delete" value="Delete" class="btn btn-sm btn-outline-danger">
+                        </form>
+                    </td>
+                    <!-- <td><a href="" class="btn btn-sm btn-outline-danger" data-toggle="modal" data-target="#exampleModal">Delete</a></td> -->
+                </tr>
+                <!-- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
@@ -87,11 +90,11 @@ mysqli_close($conn);
                                 </div>
                             </div>
                         </div> -->
-                    <?php endforeach; ?>
-                </table>
+            <?php endforeach; ?>
+        </table>
 
-            </div>
-        </div>
+    </div>
+    </div>
 </body>
 
 </html>
