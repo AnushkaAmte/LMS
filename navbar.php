d0f2d524b3b086ce3f3beb7b0ec132a236010f85
AnushkaAmte <anushkaamte@gmail.com>
2021-04-23 02:03:00 +0530

    added bg

--- navbar.php
+++ navbar.php
@@ -4,6 +4,14 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 </head>
+<style>
+    body {
+        background-color: #ffffff;
+        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cdefs%3E%3ClinearGradient id='a' x1='0' x2='0' y1='1' y2='0' gradientTransform='rotate(155,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%230FF'/%3E%3Cstop offset='1' stop-color='%23CF6'/%3E%3C/linearGradient%3E%3ClinearGradient id='b' x1='0' x2='0' y1='0' y2='1'%3E%3Cstop offset='0' stop-color='%23F00'/%3E%3Cstop offset='1' stop-color='%23FC0'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cg fill='%23FFF' fill-opacity='0' stroke-miterlimit='10'%3E%3Cg stroke='url(%23a)' stroke-width='10.56'%3E%3Cpath transform='translate(-79.1 13.600000000000001) rotate(7.3 1409 581) scale(1.0169920000000001)' d='M1409 581 1450.35 511 1490 581z'/%3E%3Ccircle stroke-width='3.5200000000000005' transform='translate(-62 50) rotate(11 800 450) scale(1.016916)' cx='500' cy='100' r='40'/%3E%3Cpath transform='translate(32.199999999999996 -114) rotate(106 401 736) scale(1.016916)' d='M400.86 735.5h-83.73c0-23.12 18.74-41.87 41.87-41.87S400.86 712.38 400.86 735.5z'/%3E%3C/g%3E%3Cg stroke='url(%23b)' stroke-width='3.2'%3E%3Cpath transform='translate(300 -12) rotate(3 150 345) scale(0.965664)' d='M149.8 345.2 118.4 389.8 149.8 434.4 181.2 389.8z'/%3E%3Crect stroke-width='7.040000000000001' transform='translate(-156 -129) rotate(122.39999999999999 1089 759)' x='1039' y='709' width='100' height='100'/%3E%3Cpath transform='translate(-255.2 80.8) rotate(20.4 1400 132) scale(0.96)' d='M1426.8 132.4 1405.7 168.8 1363.7 168.8 1342.7 132.4 1363.7 96 1405.7 96z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+        background-attachment: fixed;
+        background-size: cover;
+    }
+</style>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top ">
     <a class="navbar-brand" href="home.php">LMS</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
